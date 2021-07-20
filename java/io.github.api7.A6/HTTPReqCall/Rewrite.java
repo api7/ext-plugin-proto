6f7732255d5b2cce90d19bf0c01fe285b2ba3cec
spacewander <spacewanderlzx@gmail.com>
2021-07-20 16:38:57 +0800

    feat: recompile code with flatc 2.0.0

--- java/io.github.api7.A6/HTTPReqCall/Rewrite.java
+++ java/io.github.api7.A6/HTTPReqCall/Rewrite.java
@@ -9,7 +9,7 @@ import com.google.flatbuffers.*;
 
 @SuppressWarnings("unused")
 public final class Rewrite extends Table {
-  public static void ValidateVersion() { Constants.FLATBUFFERS_1_12_0(); }
+  public static void ValidateVersion() { Constants.FLATBUFFERS_2_0_0(); }
   public static Rewrite getRootAsRewrite(ByteBuffer _bb) { return getRootAsRewrite(_bb, new Rewrite()); }
   public static Rewrite getRootAsRewrite(ByteBuffer _bb, Rewrite obj) { _bb.order(ByteOrder.LITTLE_ENDIAN); return (obj.__assign(_bb.getInt(_bb.position()) + _bb.position(), _bb)); }
   public void __init(int _i, ByteBuffer _bb) { __reset(_i, _bb); }
